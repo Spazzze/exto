d4a6105b1eef7a9fc60555919488bac53d64b894
Spazzze <elena.space@gmail.com>
2017-09-04 16:09:17 +0300

    - reworked rx subscribers;

--- exto/src/main/java/com/github/spazzze/exto/extensions/RxExtentions.kt
+++ exto/src/main/java/com/github/spazzze/exto/extensions/RxExtentions.kt
@@ -2,46 +2,24 @@ package com.github.spazzze.exto.extensions
 
 import android.content.Context
 import com.github.spazzze.exto.errors.NoNetworkException
-import io.reactivex.*
+import io.reactivex.Flowable
+import io.reactivex.Maybe
+import io.reactivex.Observable
+import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
-import org.reactivestreams.Subscriber
-import org.reactivestreams.Subscription
 
 /**
  * @author Space
  * @date 28.01.2017
  */
 
+//region :::::::::::::::::::::::::::::::::: Single
+
 fun <T> Single<T>.withConnectionStatusCheck(context: Context): Single<T> = Single.just(context.isNetworkAvailable())
         .flatMap { if (it) this else Single.error(NoNetworkException()) }
-
-fun <T> Maybe<T>.withConnectionStatusCheck(context: Context): Maybe<T> = Maybe.just(context.isNetworkAvailable())
-        .flatMap { if (it) this else Maybe.error(NoNetworkException()) }
-
-fun <T> Observable<T>.withConnectionStatusCheck(context: Context): Observable<T> = Observable.just(context.isNetworkAvailable())
-        .flatMap { if (it) this else Observable.error(NoNetworkException()) }
-
-fun <T> Flowable<T>.withConnectionStatusCheck(context: Context): Flowable<T> = Flowable.just(context.isNetworkAvailable())
-        .flatMap { if (it) this else Flowable.error(NoNetworkException()) }
-
-fun <T> Observable<T>.runOnIoObsOnMain(): Observable<T> = this
-        .subscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-
-fun <T> Observable<T>.runOnIoObsOnIo(): Observable<T> = this
-        .subscribeOn(Schedulers.io())
-        .observeOn(Schedulers.io())
-
-fun <T> Flowable<T>.runOnIoObsOnMain(): Flowable<T> = this
-        .subscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-
-fun <T> Flowable<T>.runOnIoObsOnIo(): Flowable<T> = this
-        .subscribeOn(Schedulers.io())
-        .observeOn(Schedulers.io())
 
 fun <T> Single<T>.runOnIoObsOnMain(): Single<T> = this
         .subscribeOn(Schedulers.io())
@@ -51,6 +29,26 @@ fun <T> Single<T>.runOnIoObsOnIo(): Single<T> = this
         .subscribeOn(Schedulers.io())
         .observeOn(Schedulers.io())
 
+inline fun <reified T> Single<T>.subscribeSilent(crossinline onNextAction: (T) -> Unit) {
+    subscribe({ onNextAction(it) }, { it.reportToDeveloper("${T::class.java}") })
+}
+
+inline fun <reified T> Single<T>.subscribeReporting(crossinline onErrorAction: (Throwable) -> Unit) {
+    subscribe({ Unit }, { it.reportToDeveloper("${T::class.java}"); onErrorAction(it) })
+}
+
+inline fun <reified T> Single<T>.subscribeReporting(crossinline onNextAction: (T) -> Unit,
+                                                    crossinline onErrorAction: (Throwable) -> Unit) {
+    subscribe({ onNextAction(it) }, { it.reportToDeveloper("${T::class.java}"); onErrorAction(it) })
+}
+
+//endregion :::::::::::::::::::::::::::::::::: Single
+
+//region :::::::::::::::::::::::::::::::::: Maybe
+
+fun <T> Maybe<T>.withConnectionStatusCheck(context: Context): Maybe<T> = Maybe.just(context.isNetworkAvailable())
+        .flatMap { if (it) this else Maybe.error(NoNetworkException()) }
+
 fun <T> Maybe<T>.runOnIoObsOnMain(): Maybe<T> = this
         .subscribeOn(Schedulers.io())
         .observeOn(AndroidSchedulers.mainThread())
@@ -59,134 +57,75 @@ fun <T> Maybe<T>.runOnIoObsOnIo(): Maybe<T> = this
         .subscribeOn(Schedulers.io())
         .observeOn(Schedulers.io())
 
-inline fun <T, reified A : Any> A.silentObserver(crossinline onNextAction: (T) -> Unit) = object : Observer<T> {
-
-    override fun onSubscribe(d: Disposable) = Unit
-
-    override fun onNext(t: T) = onNextAction(t)
-
-    override fun onComplete() = Unit
-
-    override fun onError(e: Throwable) = e.reportToDeveloper("$javaClass")
+inline fun <reified T> Maybe<T>.subscribeSilent(crossinline onNextAction: (T) -> Unit) {
+    subscribe({ onNextAction(it) }, { it.reportToDeveloper("${T::class.java}") })
 }
 
-inline fun <T, reified A : Any> A.silentSubscriber(crossinline onNextAction: (T) -> Unit) = object : Subscriber<T> {
-
-    override fun onSubscribe(s: Subscription) = Unit
-
-    override fun onNext(t: T) = onNextAction(t)
-
-    override fun onComplete() = Unit
-
-    override fun onError(e: Throwable) = e.reportToDeveloper("$javaClass")
+inline fun <reified T> Maybe<T>.subscribeReporting(crossinline onErrorAction: (Throwable) -> Unit) {
+    subscribe({ Unit }, { it.reportToDeveloper("${T::class.java}"); onErrorAction(it) })
 }
 
-inline fun <T, reified A : Any> A.silentSingleObserver(crossinline onSuccessAction: (T) -> Unit) = object : SingleObserver<T> {
-
-    override fun onSubscribe(d: Disposable) = Unit
-
-    override fun onSuccess(t: T) = onSuccessAction(t)
-
-    override fun onError(e: Throwable) = e.reportToDeveloper("$javaClass")
+inline fun <reified T> Maybe<T>.subscribeReporting(crossinline onNextAction: (T) -> Unit,
+                                                   crossinline onErrorAction: (Throwable) -> Unit) {
+    subscribe({ onNextAction(it) }, { it.reportToDeveloper("${T::class.java}"); onErrorAction(it) })
 }
 
-inline fun <T, reified A : Any> A.silentMaybeObserver(crossinline onSuccessAction: (T) -> Unit) = object : MaybeObserver<T> {
+//endregion :::::::::::::::::::::::::::::::::: Maybe
 
-    override fun onComplete() = Unit
+//region :::::::::::::::::::::::::::::::::: Observable
 
-    override fun onSubscribe(d: Disposable) = Unit
+fun <T> Observable<T>.withConnectionStatusCheck(context: Context): Observable<T> = Observable.just(context.isNetworkAvailable())
+        .flatMap { if (it) this else Observable.error(NoNetworkException()) }
 
-    override fun onSuccess(t: T) = onSuccessAction(t)
+fun <T> Observable<T>.runOnIoObsOnMain(): Observable<T> = this
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
 
-    override fun onError(e: Throwable) = e.reportToDeveloper("$javaClass")
+fun <T> Observable<T>.runOnIoObsOnIo(): Observable<T> = this
+        .subscribeOn(Schedulers.io())
+        .observeOn(Schedulers.io())
+
+inline fun <reified T> Observable<T>.subscribeSilent(crossinline onNextAction: (T) -> Unit) {
+    subscribe({ onNextAction(it) }, { it.reportToDeveloper("${T::class.java}") })
 }
 
-inline fun <T, reified A : Any> A.reportingObserver(crossinline onErrorAction: (Throwable) -> Unit) = object : Observer<T> {
-
-    override fun onSubscribe(d: Disposable) = Unit
-
-    override fun onNext(t: T) = Unit
-
-    override fun onComplete() = Unit
-
-    override fun onError(e: Throwable) {
-        e.reportToDeveloper("$javaClass")
-        onErrorAction(e)
-    }
+inline fun <reified T> Observable<T>.subscribeReporting(crossinline onErrorAction: (Throwable) -> Unit) {
+    subscribe({ Unit }, { it.reportToDeveloper("${T::class.java}"); onErrorAction(it) })
 }
 
-inline fun <T, reified A : Any> A.reportingObserver(crossinline onNextAction: (T) -> Unit,
-                                                    crossinline onErrorAction: (Throwable) -> Unit) = object : Observer<T> {
-
-    override fun onSubscribe(d: Disposable) = Unit
-
-    override fun onNext(t: T) = onNextAction(t)
-
-    override fun onComplete() = Unit
-
-    override fun onError(e: Throwable) {
-        e.reportToDeveloper("$javaClass")
-        onErrorAction(e)
-    }
+inline fun <reified T> Observable<T>.subscribeReporting(crossinline onNextAction: (T) -> Unit,
+                                                        crossinline onErrorAction: (Throwable) -> Unit) {
+    subscribe({ onNextAction(it) }, { it.reportToDeveloper("${T::class.java}"); onErrorAction(it) })
 }
 
-inline fun <T, reified A : Any> A.reportingSubscriber(crossinline onErrorAction: (Throwable) -> Unit) = object : Subscriber<T> {
+//endregion :::::::::::::::::::::::::::::::::: Observable
 
-    override fun onSubscribe(s: Subscription) = Unit
+//region :::::::::::::::::::::::::::::::::: Flowable
+fun <T> Flowable<T>.withConnectionStatusCheck(context: Context): Flowable<T> = Flowable.just(context.isNetworkAvailable())
+        .flatMap { if (it) this else Flowable.error(NoNetworkException()) }
 
-    override fun onNext(t: T) = Unit
+fun <T> Flowable<T>.runOnIoObsOnMain(): Flowable<T> = this
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
 
-    override fun onComplete() = Unit
+fun <T> Flowable<T>.runOnIoObsOnIo(): Flowable<T> = this
+        .subscribeOn(Schedulers.io())
+        .observeOn(Schedulers.io())
 
-    override fun onError(e: Throwable) {
-        e.reportToDeveloper("$javaClass")
-        onErrorAction(e)
-    }
+inline fun <reified T> Flowable<T>.subscribeSilent(crossinline onNextAction: (T) -> Unit) {
+    subscribe({ onNextAction(it) }, { it.reportToDeveloper("${T::class.java}") })
 }
 
-inline fun <T, reified A : Any> A.reportingSubscriber(crossinline onNextAction: (T) -> Unit,
-                                                      crossinline onErrorAction: (Throwable) -> Unit) = object : Subscriber<T> {
-
-    override fun onSubscribe(s: Subscription) = Unit
-
-    override fun onNext(t: T) = onNextAction(t)
-
-    override fun onComplete() = Unit
-
-    override fun onError(e: Throwable) {
-        e.reportToDeveloper("$javaClass")
-        onErrorAction(e)
-    }
+inline fun <reified T> Flowable<T>.subscribeReporting(crossinline onErrorAction: (Throwable) -> Unit) {
+    subscribe({ Unit }, { it.reportToDeveloper("${T::class.java}"); onErrorAction(it) })
 }
 
-inline fun <T, reified A : Any> A.reportingMaybeObserver(crossinline onErrorAction: (Throwable) -> Unit) = object : MaybeObserver<T> {
-
-    override fun onComplete() = Unit
-
-    override fun onSubscribe(d: Disposable) = Unit
-
-    override fun onSuccess(t: T) = Unit
-
-    override fun onError(e: Throwable) {
-        e.reportToDeveloper("$javaClass")
-        onErrorAction(e)
-    }
+inline fun <reified T> Flowable<T>.subscribeReporting(crossinline onNextAction: (T) -> Unit,
+                                                      crossinline onErrorAction: (Throwable) -> Unit) {
+    subscribe({ onNextAction(it) }, { it.reportToDeveloper("${T::class.java}"); onErrorAction(it) })
 }
 
-inline fun <T, reified A : Any> A.reportingMaybeObserver(crossinline onSuccessAction: (T) -> Unit,
-                                                         crossinline onErrorAction: (Throwable) -> Unit) = object : MaybeObserver<T> {
-
-    override fun onComplete() = Unit
-
-    override fun onSubscribe(d: Disposable) = Unit
-
-    override fun onSuccess(t: T) = onSuccessAction(t)
-
-    override fun onError(e: Throwable) {
-        e.reportToDeveloper("$javaClass")
-        onErrorAction(e)
-    }
-}
+//endregion :::::::::::::::::::::::::::::::::: Flowable
 
 fun Disposable.addAsSingleInstanceTo(compositeSubscription: CompositeDisposable) = apply {
     compositeSubscription.clear()
