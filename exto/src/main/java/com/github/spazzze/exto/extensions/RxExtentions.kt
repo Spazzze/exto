b2cdbffac3dc4fbb4c6ff89df7d68fb952f26040
Spazzze <elena.space@gmail.com>
2018-02-14 14:07:32 +0300

    Revert: - switched to RxJava2

--- exto/src/main/java/com/github/spazzze/exto/extensions/RxExtentions.kt
+++ exto/src/main/java/com/github/spazzze/exto/extensions/RxExtentions.kt
@@ -2,11 +2,13 @@ package com.github.spazzze.exto.extensions
 
 import android.content.Context
 import com.github.spazzze.exto.errors.NoNetworkException
-import io.reactivex.*
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
+import rx.Observable
+import rx.Observer
+import rx.Single
+import rx.Subscription
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+import rx.subscriptions.CompositeSubscription
 
 /**
  * @author Space
@@ -16,28 +18,14 @@ import io.reactivex.schedulers.Schedulers
 fun <T> Single<T>.withConnectionStatusCheck(context: Context): Single<T> = Single.just(context.isNetworkAvailable())
         .flatMap { if (it) this else Single.error(NoNetworkException()) }
 
-fun <T> Maybe<T>.withConnectionStatusCheck(context: Context): Maybe<T> = Maybe.just(context.isNetworkAvailable())
-        .flatMap { if (it) this else Maybe.error(NoNetworkException()) }
-
 fun <T> Observable<T>.withConnectionStatusCheck(context: Context): Observable<T> = Observable.just(context.isNetworkAvailable())
         .flatMap { if (it) this else Observable.error(NoNetworkException()) }
-
-fun <T> Flowable<T>.withConnectionStatusCheck(context: Context): Flowable<T> = Flowable.just(context.isNetworkAvailable())
-        .flatMap { if (it) this else Flowable.error(NoNetworkException()) }
 
 fun <T> Observable<T>.runOnIoObsOnMain(): Observable<T> = this
         .subscribeOn(Schedulers.io())
         .observeOn(AndroidSchedulers.mainThread())
 
 fun <T> Observable<T>.runOnIoObsOnIo(): Observable<T> = this
-        .subscribeOn(Schedulers.io())
-        .observeOn(Schedulers.io())
-
-fun <T> Flowable<T>.runOnIoObsOnMain(): Flowable<T> = this
-        .subscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-
-fun <T> Flowable<T>.runOnIoObsOnIo(): Flowable<T> = this
         .subscribeOn(Schedulers.io())
         .observeOn(Schedulers.io())
 
@@ -49,53 +37,20 @@ fun <T> Single<T>.runOnIoObsOnIo(): Single<T> = this
         .subscribeOn(Schedulers.io())
         .observeOn(Schedulers.io())
 
-fun <T> Maybe<T>.runOnIoObsOnMain(): Maybe<T> = this
-        .subscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-
-fun <T> Maybe<T>.runOnIoObsOnIo(): Maybe<T> = this
-        .subscribeOn(Schedulers.io())
-        .observeOn(Schedulers.io())
-
 inline fun <T, reified A : Any> A.silentObserver(crossinline onNextAction: (T) -> Unit) = object : Observer<T> {
-
-    override fun onSubscribe(d: Disposable) = Maybe.just(1)
-            .subscribe(reportingMaybeObserver { })
 
     override fun onNext(t: T) = onNextAction(t)
 
-    override fun onComplete() = Unit
-
-    override fun onError(e: Throwable) = e.reportToDeveloper("$javaClass")
-}
-
-inline fun <T, reified A : Any> A.silentSingleObserver(crossinline onSuccessAction: (T) -> Unit) = object : SingleObserver<T> {
-
-    override fun onSubscribe(d: Disposable) = Unit
-
-    override fun onSuccess(t: T) = onSuccessAction(t)
-
-    override fun onError(e: Throwable) = e.reportToDeveloper("$javaClass")
-}
-
-inline fun <T, reified A : Any> A.silentMaybeObserver(crossinline onSuccessAction: (T) -> Unit) = object : MaybeObserver<T> {
-
-    override fun onComplete() = Unit
-
-    override fun onSubscribe(d: Disposable) = Unit
-
-    override fun onSuccess(t: T) = onSuccessAction(t)
+    override fun onCompleted() = Unit
 
     override fun onError(e: Throwable) = e.reportToDeveloper("$javaClass")
 }
 
 inline fun <T, reified A : Any> A.reportingObserver(crossinline onErrorAction: (Throwable) -> Unit) = object : Observer<T> {
 
-    override fun onSubscribe(d: Disposable) = Unit
-
     override fun onNext(t: T) = Unit
 
-    override fun onComplete() = Unit
+    override fun onCompleted() = Unit
 
     override fun onError(e: Throwable) {
         e.reportToDeveloper("$javaClass")
@@ -105,12 +60,9 @@ inline fun <T, reified A : Any> A.reportingObserver(crossinline onErrorAction: (
 
 inline fun <T, reified A : Any> A.reportingObserver(crossinline onNextAction: (T) -> Unit,
                                                     crossinline onErrorAction: (Throwable) -> Unit) = object : Observer<T> {
-
-    override fun onSubscribe(d: Disposable) = Unit
-
     override fun onNext(t: T) = onNextAction(t)
 
-    override fun onComplete() = Unit
+    override fun onCompleted() = Unit
 
     override fun onError(e: Throwable) {
         e.reportToDeveloper("$javaClass")
@@ -118,36 +70,7 @@ inline fun <T, reified A : Any> A.reportingObserver(crossinline onNextAction: (T
     }
 }
 
-inline fun <T, reified A : Any> A.reportingMaybeObserver(crossinline onErrorAction: (Throwable) -> Unit) = object : MaybeObserver<T> {
-
-    override fun onComplete() = Unit
-
-    override fun onSubscribe(d: Disposable) = Unit
-
-    override fun onSuccess(t: T) = Unit
-
-    override fun onError(e: Throwable) {
-        e.reportToDeveloper("$javaClass")
-        onErrorAction(e)
-    }
-}
-
-inline fun <T, reified A : Any> A.reportingMaybeObserver(crossinline onSuccessAction: (T) -> Unit,
-                                                         crossinline onErrorAction: (Throwable) -> Unit) = object : MaybeObserver<T> {
-
-    override fun onComplete() = Unit
-
-    override fun onSubscribe(d: Disposable) = Unit
-
-    override fun onSuccess(t: T) = onSuccessAction(t)
-
-    override fun onError(e: Throwable) {
-        e.reportToDeveloper("$javaClass")
-        onErrorAction(e)
-    }
-}
-
-fun Disposable.addAsSingleInstanceTo(compositeSubscription: CompositeDisposable) = apply {
+fun Subscription.addAsSingleInstanceTo(compositeSubscription: CompositeSubscription) = apply {
     compositeSubscription.clear()
     compositeSubscription.add(this)
 }
