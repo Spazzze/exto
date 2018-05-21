77cbc549cecf1fa12a123928838758bf10732a2d
Spazzze <elena.space@gmail.com>
2018-05-21 12:02:19 +0300

    - fragments intents comes from activity intents;

--- exto/src/main/java/com/github/spazzze/exto/extensions/ActivityExtensions.kt
+++ exto/src/main/java/com/github/spazzze/exto/extensions/ActivityExtensions.kt
@@ -51,6 +51,13 @@ fun Activity.mailTo(url: String): Boolean = try {
     false
 }
 
+fun Activity.callTo(url: String) = try {
+    val intent = Intent(Intent.ACTION_DIAL, Uri.parse(url))
+    startActivity(intent)
+} catch (e: Exception) {
+    Timber.e(e, "callTo intent error: cannot call intent ACTION_DIAL $url")
+}
+
 fun Activity.takePicture(file: File, intentId: Int) = try {
     val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
     takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file))
@@ -59,10 +66,63 @@ fun Activity.takePicture(file: File, intentId: Int) = try {
     Timber.e(e, "takePhoto intent error: ")
 }
 
+fun Activity.takeVideo(file: File, intentId: Int, maximumDurationInSeconds: Int = 0) = try {
+    val takeVideoIntent = Intent(MediaStore.ACTION_VIDEO_CAPTURE)
+    val maxDuration = if (maximumDurationInSeconds > 0) maximumDurationInSeconds else 30
+    takeVideoIntent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, maxDuration)
+    takeVideoIntent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1)
+    takeVideoIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file))
+    startActivityForResult(takeVideoIntent, intentId)
+} catch (e: Exception) {
+    Timber.e(e, "takeVideo intent error: ")
+}
+
+fun Activity.takeVideo(intentId: Int, maximumDurationInSeconds: Int = 0) = try {
+    val takeVideoIntent = Intent(MediaStore.ACTION_VIDEO_CAPTURE)
+    val maxDuration = if (maximumDurationInSeconds > 0) maximumDurationInSeconds else 30
+    takeVideoIntent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, maxDuration)
+    takeVideoIntent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1)
+    startActivityForResult(takeVideoIntent, intentId)
+} catch (e: Exception) {
+    Timber.e(e, "takeVideo intent error: ")
+}
+
 fun Activity.chooseFromGallery(@StringRes titleRes: Int, intentId: Int) = try {
     val takeFromGalleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
     takeFromGalleryIntent.type = "image/*"
     startActivityForResult(Intent.createChooser(takeFromGalleryIntent, getString(titleRes)), intentId)
 } catch (e: Exception) {
     Timber.e(e, "chooseFromGallery intent error: ")
+}
+
+fun Activity.chooseAudio(@StringRes titleRes: Int, intentId: Int) = try {
+    val intent = Intent(Intent.ACTION_GET_CONTENT)
+    intent.type = "audio/*"
+    startActivityForResult(Intent.createChooser(intent, getString(titleRes)), intentId)
+} catch (e: Exception) {
+    Timber.e(e, "chooseAudio intent error: ")
+}
+
+fun Activity.chooseMp3Audio(@StringRes titleRes: Int, intentId: Int) = try {
+    val intent = Intent(Intent.ACTION_GET_CONTENT)
+    intent.type = "audio/mpeg"
+    startActivityForResult(Intent.createChooser(intent, getString(titleRes)), intentId)
+} catch (e: Exception) {
+    Timber.e(e, "chooseMp3Audio intent error: ")
+}
+
+fun Activity.chooseVideo(@StringRes titleRes: Int, intentId: Int) = try {
+    val intent = Intent(Intent.ACTION_GET_CONTENT)
+    intent.type = "video/*"
+    startActivityForResult(Intent.createChooser(intent, getString(titleRes)), intentId)
+} catch (e: Exception) {
+    Timber.e(e, "chooseVideo intent error: ")
+}
+
+fun Activity.chooseMp4Video(@StringRes titleRes: Int, intentId: Int) = try {
+    val intent = Intent(Intent.ACTION_GET_CONTENT)
+    intent.type = "video/mp4"
+    startActivityForResult(Intent.createChooser(intent, getString(titleRes)), intentId)
+} catch (e: Exception) {
+    Timber.e(e, "chooseMp4Video intent error: ")
 }
--- exto/src/main/java/com/github/spazzze/exto/extensions/FragmentsExtensions.kt
+++ exto/src/main/java/com/github/spazzze/exto/extensions/FragmentsExtensions.kt
@@ -1,19 +1,14 @@
 package com.github.spazzze.exto.extensions
 
 import android.annotation.SuppressLint
-import android.content.Intent
-import android.net.MailTo
-import android.net.Uri
 import android.os.Bundle
 import android.os.Parcelable
-import android.provider.MediaStore
 import android.support.annotation.StringRes
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentActivity
 import android.support.v4.app.FragmentManager
 import org.funktionale.option.Option
 import org.funktionale.option.toOption
-import timber.log.Timber
 import java.io.File
 import java.io.IOException
 import java.io.Serializable
@@ -24,98 +19,25 @@ import java.io.Serializable
  * @date 29.01.2017
  */
 
-fun Fragment.mailTo(url: String): Boolean = try {
-    with(MailTo.parse(url)) {
-        val intent = Intent(Intent.ACTION_SENDTO)
-        intent.data = Uri.parse("mailto:")
-        if (to?.trim().isNotNullOrBlank()) intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(to))
-        if (subject?.trim().isNotNullOrBlank()) intent.putExtra(Intent.EXTRA_SUBJECT, subject)
-        if (body?.trim().isNotNullOrBlank()) intent.putExtra(Intent.EXTRA_TEXT, body)
-        if (intent.resolveActivity(context.packageManager) != null) {
-            startActivity(intent)
-            return true
-        }
-        return false
-    }
-} catch (e: Exception) {
-    false
-}
+fun Fragment.mailTo(url: String) = activity.mailTo(url)
 
-fun Fragment.callTo(url: String) = try {
-    val intent = Intent(Intent.ACTION_DIAL, Uri.parse(url))
-    startActivity(intent)
-} catch (e: Exception) {
-    Timber.e(e, "callTo intent error: cannot call intent ACTION_DIAL $url")
-}
+fun Fragment.callTo(url: String) = activity.callTo(url)
 
-fun Fragment.takePicture(file: File, intentId: Int) = try {
-    val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file))
-    startActivityForResult(takePictureIntent, intentId)
-} catch (e: Exception) {
-    Timber.e(e, "takePhoto intent error: ")
-}
+fun Fragment.takePicture(file: File, intentId: Int) = activity.takePicture(file, intentId)
 
-fun Fragment.takeVideo(file: File, intentId: Int, maximumDurationInSeconds: Int = 0) = try {
-    val takeVideoIntent = Intent(MediaStore.ACTION_VIDEO_CAPTURE)
-    val maxDuration = if (maximumDurationInSeconds > 0) maximumDurationInSeconds else 30
-    takeVideoIntent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, maxDuration)
-    takeVideoIntent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1)
-    takeVideoIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file))
-    startActivityForResult(takeVideoIntent, intentId)
-} catch (e: Exception) {
-    Timber.e(e, "takeVideo intent error: ")
-}
+fun Fragment.takeVideo(file: File, intentId: Int, maximumDurationInSeconds: Int = 0) = activity.takeVideo(file, intentId, maximumDurationInSeconds)
 
-fun Fragment.takeVideo(intentId: Int, maximumDurationInSeconds: Int = 0) = try {
-    val takeVideoIntent = Intent(MediaStore.ACTION_VIDEO_CAPTURE)
-    val maxDuration = if (maximumDurationInSeconds > 0) maximumDurationInSeconds else 30
-    takeVideoIntent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, maxDuration)
-    takeVideoIntent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1)
-    startActivityForResult(takeVideoIntent, intentId)
-} catch (e: Exception) {
-    Timber.e(e, "takeVideo intent error: ")
-}
+fun Fragment.takeVideo(intentId: Int, maximumDurationInSeconds: Int = 0) = activity.takeVideo(intentId, maximumDurationInSeconds)
 
-fun Fragment.chooseFromGallery(@StringRes titleRes: Int, intentId: Int) = try {
-    val takeFromGalleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-    takeFromGalleryIntent.type = "image/*"
-    startActivityForResult(Intent.createChooser(takeFromGalleryIntent, getString(titleRes)), intentId)
-} catch (e: Exception) {
-    Timber.e(e, "chooseFromGallery intent error: ")
-}
+fun Fragment.chooseFromGallery(@StringRes titleRes: Int, intentId: Int) = activity.chooseFromGallery(titleRes, intentId)
 
-fun Fragment.chooseAudio(@StringRes titleRes: Int, intentId: Int) = try {
-    val intent = Intent(Intent.ACTION_GET_CONTENT)
-    intent.type = "audio/*"
-    startActivityForResult(Intent.createChooser(intent, getString(titleRes)), intentId)
-} catch (e: Exception) {
-    Timber.e(e, "chooseAudio intent error: ")
-}
+fun Fragment.chooseAudio(@StringRes titleRes: Int, intentId: Int) = activity.chooseAudio(titleRes, intentId)
 
-fun Fragment.chooseMp3Audio(@StringRes titleRes: Int, intentId: Int) = try {
-    val intent = Intent(Intent.ACTION_GET_CONTENT)
-    intent.type = "audio/mpeg"
-    startActivityForResult(Intent.createChooser(intent, getString(titleRes)), intentId)
-} catch (e: Exception) {
-    Timber.e(e, "chooseMp3Audio intent error: ")
-}
+fun Fragment.chooseMp3Audio(@StringRes titleRes: Int, intentId: Int) = activity.chooseMp3Audio(titleRes, intentId)
 
-fun Fragment.chooseVideo(@StringRes titleRes: Int, intentId: Int) = try {
-    val intent = Intent(Intent.ACTION_GET_CONTENT)
-    intent.type = "video/*"
-    startActivityForResult(Intent.createChooser(intent, getString(titleRes)), intentId)
-} catch (e: Exception) {
-    Timber.e(e, "chooseVideo intent error: ")
-}
+fun Fragment.chooseVideo(@StringRes titleRes: Int, intentId: Int) = activity.chooseVideo(titleRes, intentId)
 
-fun Fragment.chooseMp4Video(@StringRes titleRes: Int, intentId: Int) = try {
-    val intent = Intent(Intent.ACTION_GET_CONTENT)
-    intent.type = "video/mp4"
-    startActivityForResult(Intent.createChooser(intent, getString(titleRes)), intentId)
-} catch (e: Exception) {
-    Timber.e(e, "chooseMp4Video intent error: ")
-}
+fun Fragment.chooseMp4Video(@StringRes titleRes: Int, intentId: Int) = activity.chooseMp4Video(titleRes, intentId)
 
 inline fun <T : Fragment> FragmentManager.replaceFragment(containerId: Int, addToBackStack: Boolean,
                                                           tag: String?, createNewFragment: () -> T) =
