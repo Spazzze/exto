e153089437a2827983a7b79008acfd167aab58af
Spazzze <elena.space@gmail.com>
2018-05-29 12:28:43 +0300

    - SimpleGlideLoadingListener;

---
+++ exto/src/main/java/com/github/spazzze/exto/view/SimpleGlideLoadingListener.kt
@@ -0,0 +1,23 @@
+package com.github.spazzze.exto.view
+
+import com.bumptech.glide.load.resource.drawable.GlideDrawable
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import java.lang.Exception
+
+/**
+ * @author Space
+ * @date 29.05.2018
+ */
+
+interface SimpleGlideLoadingListener<T> : RequestListener<T, GlideDrawable> {
+
+    val onLoadComplete: () -> Unit
+    val onExceptionAction: () -> Unit
+
+    override fun onResourceReady(resource: GlideDrawable?, model: T?, target: Target<GlideDrawable>?, isFromMemoryCache: Boolean, isFirstResource: Boolean) =
+            onLoadComplete().run { false }
+
+    override fun onException(e: Exception?, model: T?, target: Target<GlideDrawable>?, isFirstResource: Boolean) =
+            onExceptionAction().run { false }
+}
