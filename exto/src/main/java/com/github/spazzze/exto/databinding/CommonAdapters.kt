160e1afe939b15e4ed3f81cfdb941031fe97d96a
Travis <travis@travis-ci.org>
2020-04-03 14:15:50 +0300

    - update visibility adapter;

--- exto/src/main/java/com/github/spazzze/exto/databinding/CommonAdapters.kt
+++ exto/src/main/java/com/github/spazzze/exto/databinding/CommonAdapters.kt
@@ -3,6 +3,7 @@ package com.github.spazzze.exto.databinding
 import androidx.databinding.BindingAdapter
 import androidx.databinding.BindingConversion
 import android.view.View
+import androidx.databinding.ObservableBoolean
 
 /**
  * @author elena
@@ -11,6 +12,9 @@ import android.view.View
 
 @BindingConversion
 fun convertBooleanToVisibility(visible: Boolean) = if (visible) View.VISIBLE else View.GONE
+
+@BindingConversion
+fun convertBooleanToVisibility(visible: ObservableBoolean) = if (visible.get()) View.VISIBLE else View.GONE
 
 @BindingAdapter("onClick")
 fun bindOnClick(view: View, runnable: Runnable) {
